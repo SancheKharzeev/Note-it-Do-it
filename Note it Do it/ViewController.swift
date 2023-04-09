a629aef50c9bda73f5e5c5ce56717150fd095181
Aleksandr Kh <akumasanta@gmail.com>
2023-04-09 17:01:52 +0600

    Initial Commit

---
+++ Note it Do it/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Note it Do it
+//
+//  Created by Александр Х on 09.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
